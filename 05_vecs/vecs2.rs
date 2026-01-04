a8f1e5ab93f19420e8bd7b3bd1b0c3332aff1ec1
euanlimzx <euanlimzx@gmail.com>
2026-01-04 15:41:35 -0800

    feat: 05 sols

--- 05_vecs/vecs2.rs
+++ 05_vecs/vecs2.rs
@@ -1,9 +1,7 @@
 fn vec_loop(input: &[i32]) -> Vec<i32> {
     let mut output = Vec::new();
-
-    for element in input {
-        // TODO: Multiply each element in the `input` slice by 2 and push it to
-        // the `output` vector.
+    for i in input{
+        output.push(i * 2);
     }
 
     output
@@ -23,9 +21,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     // See the example in the function `vec_map_example` above.
     input
         .iter()
-        .map(|element| {
-            // ???
-        })
+        .map(|element | element * 2 )
         .collect()
 }
 
