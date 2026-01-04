f4445bbf183162128a1a45aef07f75b972dd3ca6
euanlimzx <euanlimzx@gmail.com>
2026-01-04 11:45:56 -0800

    feat: 04 sols

--- quizzes/quiz1.rs
+++ quizzes/quiz1.rs
@@ -14,7 +14,15 @@
 
 fn main() {
     // You can optionally experiment here.
+    
 }
+fn calculate_price_of_apples(num_apples: i32) -> i32{
+        if num_apples > 40 {
+            num_apples
+        } else {
+            num_apples * 2 
+        }
+    }
 
 // Don't change the tests!
 #[cfg(test)]
