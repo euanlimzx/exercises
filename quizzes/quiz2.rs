bc4e006e91d72351fe98d8fa5848aabd62405eda
euanlimzx <euanlimzx@gmail.com>
2026-01-05 16:40:41 -0800

    feat: 10 sols

--- quizzes/quiz2.rs
+++ quizzes/quiz2.rs
@@ -28,6 +28,21 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String,Command)>)->Vec<String> {
+        let mut res: Vec<String> = Vec::new();
+        for e in input {
+            let s = e.0;
+            let c = e.1;
+            match c {
+                Command::Uppercase=>{res.push(s.to_uppercase())},
+                Command::Trim=>{res.push(s.trim().to_owned())},
+                Command::Append(usize)=>{
+                    res.push(s + &"bar".repeat(usize))
+                },
+            }
+        }
+        res
+    }
 }
 
 fn main() {
@@ -37,7 +52,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
